1c912b7ce49d2342e029d4da8b955d0cceab9d5b
johneybi <eun082971@gmail.com>
2025-12-02 10:47:38 +0900

    upload

---
+++ 1202_03.js
@@ -0,0 +1,21 @@
+const puppy = {
+    name: "puppy",
+    age: 3,
+    breed: "french bulldog" 
+}
+
+console.log(puppy)
+console.log(puppy.name)
+console.log(puppy.age)
+console.log(puppy.breed)
+console.log(puppy[type])
+
+
+puppy.age = 5;
+console.log(puppy.age)
+
+puppy.color = "brindle"
+console.log(puppy)
+
+delete puppy.color
+console.log(puppy)
